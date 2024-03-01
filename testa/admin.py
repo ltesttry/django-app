2e8ca7e88212bb4427a77e27be7350de8f3434c4
lage <lagetest123456@outlook.com>
2024-03-01 10:27:05 +0100

    gdfg

--- testa/admin.py
+++ testa/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin, Question, Choice
-from .models import Notes
+from django.contrib import admin
+from .models import Notes, Question, Choice
 
 # Register your models here.
 admin.site.register(Question)
