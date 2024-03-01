32d6457b4cfa3b4e43ccf73b42e209bf78f17168
lage <lagetest123456@outlook.com>
2024-03-01 09:08:33 +0100

    admin and shit

--- testa/admin.py
+++ testa/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Question, Notes, Choice
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Notes)
+admin.site.register(Choice)
