cc6c66706440194ed5e51c3f2d0a6010f92f36da
lage <lagetest123456@outlook.com>
2024-03-01 18:19:42 +0100

    dasd

--- testa/models.py
+++ testa/models.py
@@ -9,20 +9,10 @@ from django.utils import timezone
 
 class Notes(models.Model):
     note = models.CharField(max_length=500)
-    date = models.DateField()
-
-    def __str__(self):
-        return self.note
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
 
-    def __str__(self):
-        return self.question_text
-    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 """class Choice(models.Model):
