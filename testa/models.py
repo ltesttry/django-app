a5a6ebf07475111237a841defb18502644c4caeb
lage <lagetest123456@outlook.com>
2024-03-02 16:06:34 +0100

    asdad

--- testa/models.py
+++ testa/models.py
@@ -10,9 +10,17 @@ from django.utils import timezone
 class Notes(models.Model):
     note = models.CharField(max_length=500)
     date = models.DateField()
-    test1 = models.CharField(max_length=500, default="")
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- testa/urls.py
+++ testa/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from . import views
 
 urlpatterns = [
-    path("", index, name="home"),
+    path("", views.index, name="home"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
